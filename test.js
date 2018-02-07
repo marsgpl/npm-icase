aa43de208ea9f083f4a0bd3c0c05e8bcb0ca9b25
marsgpl <marsgpl@gmail.com>
2018-02-07 22:37:45 +0300

    readme

--- test.js
+++ test.js
@@ -7,5 +7,5 @@ const icase = require("./icase")
 const tomatoes = ["томатов", "томат", "томата"]
 
 for ( let amount=-5; amount<=22; ++amount ) {
-    console.log(amount, tomatoes[icase(amount)])
+    console.log(amount + " " + tomatoes[icase(amount)])
 }
