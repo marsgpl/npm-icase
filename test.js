15756b8a7aa4e77dfb3224331ef28554fd26cbe6
marsgpl <marsgpl@gmail.com>
2018-02-07 22:31:08 +0300

    readme

--- test.js
+++ test.js
@@ -8,6 +8,6 @@ const cases = ["томатов", "томат", "томата"]
 
 const length = process.argv[2] || 30
 
-Array.apply(null, { length }).map(Number.call, Number).forEach(amount => {
+for ( let amount=-10; amount<=length; ++amount ) {
     console.log(amount + " " + cases[icase(amount)])
-})
+}
